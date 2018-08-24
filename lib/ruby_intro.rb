ae36b06c525305107c3bd6b7e095a4257414f876
EC2 Default User <ec2-user@ip-172-31-30-47.us-east-2.compute.internal>
2018-08-24 02:34:05 +0000

    Passed all spec

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,63 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.reduce(0,:+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  neg_inf = -1*Float::INFINITY
+  max_2 = arr.reduce([neg_inf, neg_inf]) do |acc, n|
+    if n >= acc[0]
+      acc[1] = acc[0]
+      acc[0] = n
+    elsif n > acc[1]
+      acc[1] = n
+    end
+    acc
+  end
+  (0..1).each { |n| max_2[n] = 0 if max_2[n] == neg_inf }
+  max_2[0] + max_2[1]
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  sorted = arr.sort
+  ascend_cursor = 0
+  descend_cursor = sorted.length - 1
+  while ascend_cursor < descend_cursor
+    sum = sorted[ascend_cursor] + sorted[descend_cursor]
+    return true if sum == n
+    ascend_cursor += 1 if sum < n
+    descend_cursor -= 1 if sum > n
+  end
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /^[B-DF-HJ-NP-TV-Zb-df-hj-np-tv-z]/.match s
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  /^[10]*100$|^0$/.match s
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    raise ArgumentError if isbn == '' || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  
+  attr_accessor :isbn, :price
+  
+  def price_as_string
+    "$%0.2f" % @price
+  end
 end
